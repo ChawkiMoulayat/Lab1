09745723f419c53416215ed749a0abb1aa6995d8
Chawki Moulayat <moul0084@algonquinlive.com>
2023-06-06 19:18:41 -0400

    Initiation Task 2 of Week 4

--- app/src/main/java/algonquin/cst2335/moul0084/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/moul0084/MainActivity.java
@@ -2,7 +2,11 @@ package algonquin.cst2335.moul0084;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +18,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imgView;
     Switch sw;
@@ -21,49 +27,67 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Log.w( "MainActivity", "In onCreate() - The application is now responding to user input" );
+        Log.w("MainActivity", "In onResume() - The application is now responding to user input");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.w( "MainActivity", "In onCreate() -  The application no longer responds to user input." );
+        Log.w("MainActivity", "In onPause() - The application no longer responds to user input.");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.w( "MainActivity", "In onCreate() - The application is no longer visible." );
+        Log.w("MainActivity", "In onStop() - The application is no longer visible.");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.w( "MainActivity", "In onCreate() - Any memory used by the application is freed." );
+        Log.w("MainActivity", "In onDestroy() - Any memory used by the application is freed.");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.w( "MainActivity", "In onCreate() - The application is now visible on screen." );
-
+        Log.w("MainActivity", "In onStart() - The application is now visible on screen.");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        SharedPreferences sharedPreferences  = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = sharedPreferences .getString("LoginName", "");
 
-        Log.w( "MainActivity", "In onCreate() - is the first function that gets created when an application is launched." );
+        Log.w("MainActivity", "In onCreate() - Loading Widgets");
+
+        Log.w("MainActivity", "In onCreate() - This is the first function that gets created when an application is launched.");
         Button button = findViewById(R.id.loginButton);
         EditText emailEditText = findViewById(R.id.emailEditText);
+        emailEditText.setText(emailAddress);
         button.setOnClickListener(clk -> {
             // Perform the action when the loginButton is clicked
             // Launch the second activity
             Intent secondIntent = new Intent(MainActivity.this, SecondActivity.class);
-            secondIntent.putExtra( "EmailAddress", emailEditText.getText().toString() );
+            String email = emailEditText.getText().toString();
+            SharedPreferences prefs;
+            prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+            secondIntent.putExtra("EmailAddress", emailEditText.getText().toString());
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("LoginName", email);
+            editor.putFloat("hi" , 6.0f);
+            editor.putInt("age" , 35);
+            editor.apply();
             startActivity(secondIntent);
         });
+
+        String filename = "Picture.png";
+        File file = new File(getFilesDir(), filename);
+
+        if (file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(file.getAbsolutePath());
+        }
     }
 }
--- app/src/main/java/algonquin/cst2335/moul0084/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/moul0084/SecondActivity.java
@@ -4,44 +4,85 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class SecondActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView profileImage;
+    private ActivityResultLauncher<Intent> cameraResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String phoneNumber = prefs.getString("PhoneNumber", "");
+        EditText editTextPhone = findViewById(R.id.editTextPhone);
+        editTextPhone.setText(phoneNumber);
         TextView emailTextView = findViewById(R.id.textView);
         Intent fromPrevious = getIntent();
         String emailAddress = fromPrevious.getStringExtra("EmailAddress");
         emailTextView.setText("Welcome back " + emailAddress);
-        Log.w( "SecondActivity", "In onCreate() - emailTextView" );
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+
+        profileImage = findViewById(R.id.profileImage);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        cameraResult = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
+                        if (result.getResultCode() == RESULT_OK) {
                             Intent data = result.getData();
-                            Bitmap thumbnail = data.getParcelableExtra("data");
-                            ImageView profileImage = null;
-                            profileImage.setImageBitmap (thumbnail);
+                            Bundle extras = data.getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            profileImage.setImageBitmap(imageBitmap);
                         }
                     }
                 });
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
 
-        cameraResult.launch(cameraIntent);
+        EditText editTextPhone = findViewById(R.id.editTextPhone);
+        String phoneNumber = editTextPhone.getText().toString();
+
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("PhoneNumber", phoneNumber);
+        editor.apply();
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            cameraResult.launch(takePictureIntent);
+        }
     }
 }
