67670098b1cc48f5985efb8267633517e7bc9186
Chawki Moulayat <moul0084@algonquinlive.com>
2023-08-01 15:17:01 -0400

    Finished Week10.

--- app/src/main/java/algonquin/cst2335/moul0084/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/moul0084/MainActivity.java
@@ -1,13 +1,27 @@
 package algonquin.cst2335.moul0084;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import algonquin.cst2335.moul0084.databinding.ActivityMainBinding;
 
@@ -18,90 +32,80 @@ import algonquin.cst2335.moul0084.databinding.ActivityMainBinding;
  * Version : 1.0
  */
 public class MainActivity extends AppCompatActivity {
-    /**
-     * The binding object for the activity's layout.
-     */
-    private ActivityMainBinding binding;
 
-    /**
-     * The TextView for displaying text.
-     */
-    private TextView tv = null ;
-
-    /**
-     * The EditText for user input.
-     */
-    private EditText et = null ;
-
-    /**
-     * The Button for triggering the password complexity check.
-     */
-    private Button btn = null ;
-    /**
-     * Called when the activity is starting.
-     *
-     * @param savedInstanceState Bundle object.
-     */
-    @Override
+    protected String cityName;
+    protected String apiKey;
+    protected RequestQueue queue = null;
+    String description = null;
+    String iconName = null;
+    double current = 0;
+    double max = 0;
+    double min = 0;
+    int humidity = 0;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Initialize views
-        tv = binding.textView4;
-        et = binding.editText1;
-        btn = binding.button;
-
-        // Set click listener for the button
-        btn.setOnClickListener(clk -> {
-            String password = et.getText().toString();
-            passwordComplexity(password);
-        });
-    }
-
-    /**
-     * Checks the complexity of the provided password and displays corresponding Toast messages.
-     *
-     * @param password The password to check.
-     */
-    void passwordComplexity(String password) {
-        boolean hasUppercase = false;
-        boolean hasLowercase = false;
-        boolean hasNumber = false;
-        boolean hasSpecialSymbol = false;
-        boolean missingRequirement = false;
-
-        for (char ch : password.toCharArray()) {
-            if (Character.isUpperCase(ch)) {
-                hasUppercase = true;
-            } else if (Character.isLowerCase(ch)) {
-                hasLowercase = true;
-            } else if (Character.isDigit(ch)) {
-                hasNumber = true;
-            } else if ("#$%^&*!@?".contains(String.valueOf(ch))) {
-                hasSpecialSymbol = true;
-            } else {
-                missingRequirement = true;
+        queue = Volley.newRequestQueue(this);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding.btnForecast.setOnClickListener(click -> {
+            cityName = binding.etCity.getText().toString();
+            apiKey = "7e943c97096a9784391a981c4d878b22";
+            String url = null;
+            try {
+                url = "https://api.openweathermap.org/data/2.5/weather?q="
+                        + URLEncoder.encode(cityName, "UTF-8")
+                        + "&appid=" + apiKey + "&units=metric";
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
             }
-        }
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    (response) -> {
+                        try {
+                            JSONArray weatherArray = response.getJSONArray("weather");
+                            JSONObject position0 = weatherArray.getJSONObject(0);
+                            description = position0.getString("description");
+                            iconName = position0.getString("icon");
+                            JSONObject mainObject = response.getJSONObject("main");
+                            current = mainObject.getDouble("temp");
+                            min = mainObject.getDouble("temp_min");
+                            max = mainObject.getDouble("temp_max");
+                            humidity = mainObject.getInt("humidity");
+                            String imageUrl = "http://openweathermap.org/img/w/"+ iconName + ".png";
+                            ImageRequest imgReq = new ImageRequest(imageUrl, (bitmap) -> {
+                                try {
+                                    FileOutputStream fOut = openFileOutput( iconName + ".png", Context.MODE_PRIVATE);
+                                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                    fOut.flush();
+                                    fOut.close();
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                runOnUiThread(() -> {
+                                    binding.icon.setImageBitmap(bitmap);
+                                    binding.icon.setVisibility(View.VISIBLE);
+                                });
+                            }, 1024, 1024, ImageView.ScaleType.CENTER, null,
+                                    (error) -> {});
+                            queue.add(imgReq);
+                            runOnUiThread(() -> {
+                                binding.temp.setText("The current temperature is " + current);
+                                binding.temp.setVisibility(View.VISIBLE);
+                                binding.maxTemp.setText("The max temperature is " + max);
+                                binding.maxTemp.setVisibility(View.VISIBLE);
+                                binding.minTemp.setText("The min temperature is " + min);
+                                binding.minTemp.setVisibility(View.VISIBLE);
+                                binding.humidity.setText("The humidity is " + humidity);
+                                binding.humidity.setVisibility(View.VISIBLE);
+                                binding.description.setText(description);
+                                binding.description.setVisibility(View.VISIBLE);
 
-        // Display Toast messages based on password complexity
-        if (!hasUppercase) {
-            Toast.makeText(MainActivity.this, "Your password does not have an uppercase letter", Toast.LENGTH_SHORT).show();
-            tv.setText("You shall not pass");
-        } else if (!hasLowercase) {
-            Toast.makeText(this, "Your password does not have a lowercase letter", Toast.LENGTH_SHORT).show();
-            tv.setText("You shall not pass");
-        } else if (!hasNumber) {
-            Toast.makeText(this, "Your password does not have a number", Toast.LENGTH_SHORT).show();
-            tv.setText("You shall not pass");
-        } else if (!hasSpecialSymbol) {
-            Toast.makeText(this, "Your password does not have a special symbol", Toast.LENGTH_SHORT).show();
-            tv.setText("You shall not pass");
-        } else if (!missingRequirement) {
-            Toast.makeText(this, "Your password meets all requirements", Toast.LENGTH_SHORT).show();
-            tv.setText("Your password is complex enough");
-        }
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    },
+                    (error) -> {});
+            queue.add(request);
+        });
     }
 }
